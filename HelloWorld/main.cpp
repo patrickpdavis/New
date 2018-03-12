03af09546569479818bdca11bfb25324d8c21c74
Patrick Davis <patrickpdavis@gmail.com>
2018-03-12 06:40:08 -0400

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Patrick Davis on 3/12/18.
+//  Copyright © 2018 Patrick Davis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
